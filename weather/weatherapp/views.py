248bf2bbfa21f70c137a06d0a1c8437a108a2fd3
EMMMABK <adilbikiev2@gmail.com>
2025-01-15 20:56:11 +0600

    add Final Commit

--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -1,53 +1,51 @@
 from django.shortcuts import render
+from django.contrib import messages
 import requests
 import datetime
+from decouple import config
 
-# Create your views here.
-def index(request):
-    API_KEY = open("weather/API_KEY", "r").read()
-    current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
-    forecast_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}"
-
-    if request.method == 'POST':
-        city1 = request.POST['city1']
-        city2 = request.POST.get('city2', None)
-
-        weather_data1, daily_forecasts1 = fetch_weather_and_forecast(city1, API_KEY, current_weather_url, forecast_url)
-        if city2 :
-            weather_data2, daily_forecasts2 = fetch_weather_and_forecast(city2, API_KEY, current_weather_url, forecast_url)
-        else:
-            weather_data2, daily_forecasts2 = None, None
-        
-        context = {
-            "weather_data1": weather_data1,
-            "daily_forecasts1": daily_forecasts1,
-            "weather_data2": weather_data2,
-            "daily_forecasts2": daily_forecasts2
-        }
-        return render(request, "weatherapp/index.html", context)
+def home(request):
+   
+    if 'city' in request.POST:
+         city = request.POST['city']
     else:
-        return render(request, "weatherapp/index.html")
+         city = 'New York'     
     
-def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url):
-    response = requests.get(current_weather_url.format(city,api_key)).json()
-    lat, lon = response['coord', 'lat'], response['coord', 'lon']
-    forecast_response = requests.get(forecast_url.format(lat,lon, api_key)).json()
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={config('API')}'
+    PARAMS = {'units':'metric'}
 
-    weather_data = {
-        "city": city,
-        "temperature": round(response['main']['temp'] - 273.15, 2),
-        "description": response['weather'][0]['description'],
-        "icon": response['weather'][0]['icon']
-    }
+    API_KEY = config('API_KEY')
 
-    daily_forecasts = []
-    for daily_data in forecast_response['daily'][:5]:
-        daily_forecasts.append({
-            "day": datetime.datetime.fromtimestamp(daily_data['dt']).strftime("%A"),
-            "min_temp": round(daily_data['temp']['min'] - 273.15, 2),
-            "max_temp": round(daily_data['temp']['max'] - 273.15, 2),
-            "description": daily_data['weather'][0]['description'],
-            "icon": daily_data['weather'][0]['icon']
-        })
+    SEARCH_ENGINE_ID = config('SEARCH_ENGINE_ID')
+     
+    query = city
+    page = 1
+    start = (page - 1) * 10 + 1
+    searchType = 'image'
+    city_url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}&searchType={searchType}&imgSize=xlarge"
 
-    return weather_data, daily_forecasts
+    data = requests.get(city_url).json()
+    count = 1
+    search_items = data.get("items")
+    image_url = search_items[1]['link']
+    
+
+    try:
+          
+          data = requests.get(url,params=PARAMS).json()
+          description = data['weather'][0]['description']
+          icon = data['weather'][0]['icon']
+          temp = data['main']['temp']
+          day = datetime.date.today()
+
+          return render(request,'weatherapp/index.html' , {'description':description , 'icon':icon ,'temp':temp , 'day':day , 'city':city , 'exception_occurred':False ,'image_url':image_url})
+    
+    except KeyError:
+          exception_occurred = True
+          messages.error(request,'Entered data is not available to API')   
+          day = datetime.date.today()
+
+          return render(request,'weatherapp/index.html' ,{'description':'clear sky', 'icon':'01d'  ,'temp':0 , 'day':day , 'city':'New York' , 'exception_occurred':exception_occurred } )
+               
+    
+    
