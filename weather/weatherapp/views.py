3d581f7fe66c46bb70f33f1851e15456d434d43c
EMMMABK <adilbikiev2@gmail.com>
2025-01-15 18:10:24 +0600

    add Backend Dev

--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -11,6 +11,20 @@ def index(request):
     if request.method == 'POST':
         city1 = request.POST['city1']
         city2 = request.get('city2', None)
+
+        weather_data1, daily_forecasts1 = fetch_weather_and_forecast(city1, API_KEY, current_weather_url, forecast_url)
+        if city2 :
+            weather_data2, daily_forecasts2 = fetch_weather_and_forecast(city2, API_KEY, current_weather_url, forecast_url)
+        else:
+            weather_data2, daily_forecasts2 = None, None
+        
+        context = {
+            "weather_data1": weather_data1,
+            "daily_forecasts1": daily_forecasts1,
+            "weather_data2": weather_data2,
+            "daily_forecasts2": daily_forecasts2
+        }
+        return render(request, "weatherapp/index.html", context)
     else:
         return render(request, "weatherapp/index.html")
     
@@ -29,5 +43,11 @@ def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url)
     daily_forecasts = []
     for daily_data in forecast_response['daily'][:5]:
         daily_forecasts.append({
-            "day": datetime.datetime.fromtimestamp(daily_data['dt']).strftime("%A")
+            "day": datetime.datetime.fromtimestamp(daily_data['dt']).strftime("%A"),
+            "min_temp": round(daily_data['temp']['min'] - 273.15, 2),
+            "max_temp": round(daily_data['temp']['max'] - 273.15, 2),
+            "description": daily_data['weather'][0]['description'],
+            "icon": daily_data['weather'][0]['icon']
         })
+
+    return weather_data, daily_forecasts
